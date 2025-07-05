ab745874922407edf8e471e923852f2e14973257
Agenorwot patience <patienceagenorwot631@gmail.com>
2025-07-05 22:23:30 +0300

    defined the second multiplication function

--- multiply.py
+++ multiply.py
@@ -1,3 +1,5 @@
-"""from multiply import multiply
+from multiply import multiply
 def test_multiply_1_by_1():
-    assert multiply(1, 1) == 1"""
+    assert multiply(1, 1) == 1
+
+    
--- tests/test_multiply.py
+++ tests/test_multiply.py
@@ -3,3 +3,7 @@ def multiply(a, b):
 
 def test_multiply_1_by_1():
     assert multiply(1, 1) == 1  
+
+    def test_multiply_2_by_2():
+        assert multiply(2, 2) == 4
+        
