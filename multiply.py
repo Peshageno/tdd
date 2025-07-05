0b3b91f70128eb196d81be6f5e135e0dccca804d
Agenorwot patience <patienceagenorwot631@gmail.com>
2025-07-05 22:04:40 +0300

    defined the first multiplication function

---
+++ multiply.py
@@ -0,0 +1,3 @@
+"""from multiply import multiply
+def test_multiply_1_by_1():
+    assert multiply(1, 1) == 1"""
---
+++ tests/test_multiply.py
@@ -0,0 +1,5 @@
+def multiply(a, b):
+    return a * b
+
+def test_multiply_1_by_1():
+    assert multiply(1, 1) == 1  
