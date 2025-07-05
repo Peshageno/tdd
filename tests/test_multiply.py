6126e251596057f361de650f0746796925d6967c
Agenorwot patience <patienceagenorwot631@gmail.com>
2025-07-05 22:49:25 +0300

    defined the fifth multiplication function

--- tests/test_multiply.py
+++ tests/test_multiply.py
@@ -5,16 +5,16 @@ def test_multiply_1_by_1():
     assert multiply(1, 1) == 1  
 
 
-    def test_multiply_2_by_2():
-        assert multiply(2, 2) == 4
+def test_multiply_2_by_2():
+    assert multiply(2, 2) == 4
 
 
-    def test_multiply_3_by_3():
-        assert multiply(3, 3) == 9
+def test_multiply_3_by_3():
+    assert multiply(3, 3) == 9
 
 
-        def test_multiply_4_by_4():
-            assert multiply(4, 4) == 16
+def test_multiply_4_by_4():
+    assert multiply(4, 4) == 16
 
 def test_multiply_23_by_45():
     assert multiply(23, 45) == 23 * 45
