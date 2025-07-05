5b532c6f18b64e7702ba51d9941e5330ee321920
Agenorwot patience <patienceagenorwot631@gmail.com>
2025-07-05 22:37:47 +0300

    defined the fourth multiplication function

--- tests/test_multiply.py
+++ tests/test_multiply.py
@@ -4,8 +4,15 @@ def multiply(a, b):
 def test_multiply_1_by_1():
     assert multiply(1, 1) == 1  
 
+
     def test_multiply_2_by_2():
         assert multiply(2, 2) == 4
 
+
     def test_multiply_3_by_3():
         assert multiply(3, 3) == 9
+
+
+        def test_multiply_4_by_4():
+            assert multiply(4, 4) == 16
+
